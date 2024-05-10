918b40def88d0929ddade6fb8d1253e2b44162cd
SuyashGautam05 <gautamsuyash5@gmail.com>
2024-05-10 15:54:27 +0530

    Added edge case in the upper function

--- sumOfDigit.py
+++ sumOfDigit.py
@@ -3,14 +3,17 @@ sum_of_Digit = 0
 while num != 0:
     sum_of_Digit += num % 10
     num = num // 10
+    if sum_of_Digit>=10 and num == 0:
+        num = sum_of_Digit
+        sum_of_Digit = 0
 print(f"Sum of digit of the given number, {sum_of_Digit}")
 
+
+#------------------------------------------------------------------------------------------------------------------------------------------------->
 
 
 num1 = 8.1
 num2 = 7.2
 sum = num1 + num2
 print(f"The sum of {num1} and {num2} is , {sum}")
-print(f"The sum of {0} and {1} is , {sum}")
-
-
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
