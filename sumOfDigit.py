29f8d6bf129bc65a985e3952e512c86b6f59dc81
SuyashGautam05 <gautamsuyash5@gmail.com>
2024-05-10 14:38:49 +0530

    Program of Sum of Digit

---
+++ sumOfDigit.py
@@ -0,0 +1,16 @@
+num = int(input("Enter a number:"))
+sum_of_Digit = 0
+while num != 0:
+    sum_of_Digit += num % 10
+    num = num // 10
+print(f"Sum of digit of the given number, {sum_of_Digit}")
+
+
+
+num1 = 8.1
+num2 = 7.2
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is , {sum}")
+print(f"The sum of {0} and {1} is , {sum}")
+
+
